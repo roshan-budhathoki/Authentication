0d4a0f41be57a23c7b84a8d78c2340e4cefe4e8b
Raju Choudhary <52706390+razoo-choudhary@users.noreply.github.com>
2021-04-27 17:17:43 +0545

    query
    query

---
+++ query/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$con = mysqli_connect('localhost','root','','ee');
+
+$query = mysqli_query($con,"SELECT * FROM products");
+
+
+// $result= mysqli_fetch_assoc($query);
+
+$result= mysqli_fetch_all($query,MYSQLI_ASSOC);
+
+foreach ($result as $key) {
+    
+
+echo $key['image'];
+
+}
+
+
+//var_dump($result);
+// while($r = mysqli_fetch_assoc($query)){
+
+
+// }
+
+
+
+?>
