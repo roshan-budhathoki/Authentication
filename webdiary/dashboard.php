73450ba30ba5b78c1d9bfe7de405b8f3e6704540
Roshan Budhathoki <47130986+Quicker123@users.noreply.github.com>
2021-05-09 20:13:41 +0545

    updated dashboard

--- webdiary/dashboard.php
+++ webdiary/dashboard.php
@@ -1,22 +1,28 @@
 <?php
     include "config.php";
-    if(isset($_POST['submit'])){
-    $value = $_COOKIE['newUser'];
-    $query = mysqli_query($connect, "SELECT * FROM user WHERE user_id = '$value'");
-    $result = mysqli_fetch_assoc($query);
-    $details = mysqli_query($connect, "SELECT * FROM details WHERE user_id = '$value'");
-    echo "your name is " . $result['user_name'];
     
-        if(mysqli_num_rows($details) > 0){
-            echo "you response has already been recorded";
-        }else{  
-        $response = $_POST['response'];
-        $enter = mysqli_query($connect, "INSERT INTO details (detail_description, user_id) VALUES ('$response', '$value')");
-        if($enter){
-            echo  "Thank you for your response" ;
-        }
+    if(isset($_COOKIE["newUser"])){
+        if(isset($_POST['submit'])){
+            $value = $_COOKIE['newUser'];
+            $query = mysqli_query($connect, "SELECT * FROM user WHERE user_id = '$value'");
+            $result = mysqli_fetch_assoc($query);
+            $details = mysqli_query($connect, "SELECT * FROM details WHERE user_id = '$value'");
+            echo "your name is " . $result['user_name'];
+            
+                if(mysqli_num_rows($details) > 0){
+                    echo "you response has already been recorded";
+                }else{  
+                $response = $_POST['response'];
+                $enter = mysqli_query($connect, "INSERT INTO details (detail_description, user_id) VALUES ('$response', '$value')");
+                if($enter){
+                    echo  "Thank you for your response" ;
+                }
+            }
+            }
+    }else{
+        header('location: login.php');
     }
-}
+        
 ?>
 
 <html>
