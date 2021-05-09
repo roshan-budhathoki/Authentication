1e15365649a2b361ad056c172471e843b34b3b2a
Raju Choudhary <52706390+razoo-choudhary@users.noreply.github.com>
2021-05-09 18:57:35 +0545

    ff
    ff

--- webdiary/login.php
+++ webdiary/login.php
@@ -8,22 +8,63 @@
         if(empty($username) || empty($password)){
             echo ("Please enter all the value.");
         }else{
+
+            //hash password
+
             $hassedPassword = md5($password);
-            $query = mysqli_query($connect, "SELECT * FROM user WHERE user_name = '$username' AND user_password = '$hassedPassword'");
-            $result = mysqli_fetch_assoc($query);
+            
+
+            //Check user Existance
+
+            $query = mysqli_query($connect, "SELECT * FROM users WHERE username = '$username' AND password = '$hassedPassword'");
+        
             if(mysqli_num_rows($query) > 0){
-                    setcookie("newUsers", $result['user_id'], time() + ( 7 * 24 * 60 * 60 ));
-                    $values = $_COOKIE["newUsers"];
-                    echo $values;
-                    // $randomValue = rand(1000, 10000);
-                    // $checker = mysqli_query($connect, "INSERT INTO twofactor (user_number, code) VALUES ('$values', '$randomValue')");
-                    // if($checker){
-                    //     header('location: twofactorauth.php');
-                    // }
-                    // else{
-                    //     echo "Code is not generated.";
-                    // }
+
+                //Fetch user id only when user exists.
+
+                $result = mysqli_fetch_assoc($query);
+
+
+                //generate random key
+
+                    $randomValue = rand(1000, 10000);
+
+                //store into data base {YOU ARE STORING USER ID INTO THE COOKIES } So {USER ID IS SAME AS COOKIE VALUE}  THATS WHY {WE WILL STORE USRE ID AND CREATE COOKIE ACCORDING TO ID}
+
+                    $checker = mysqli_query($connect, "INSERT INTO products (user_number, code) VALUES ('{$result['user_id']}', '$randomValue')");
+                    
+                    if($checker){
+                    
+
+                        //CREATE COOKIE WITH THE SAME USER ID WE STROED {CREATE COOKIE ONLY WHEN EVERY THING SUCCEED}
+
+                        setcookie("newUsers", $result['user_id'], time() + ( 7 * 24 * 60 * 60 ));
+
+                        header('location: twofactorauth.php');
+
+
+                        /*
+                        NOTE;
+
+                        $value = setcookie("newUsers", $result['user_id'], time() + ( 7 * 24 * 60 * 60 ));
+
+                        $value will return 1 becaues it will check isset
+
+                        $value = $_COOKIE['newUsers']
+
+                        $value will return 0 because cookie is not yet created
+                        
+                        */
+                    
+                    }
+                    
+                    else{
+                    
+                        echo "Code is not generated.";
+                    
+                    }
             }else{
+
                 echo "no such user found";
             }
         }
