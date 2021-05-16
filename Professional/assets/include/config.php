2220944ad8495249125367fc99311fc641619de2
Roshan Budhathoki <47130986+Quicker123@users.noreply.github.com>
2021-05-16 08:44:03 +0545

    change

--- Professional/assets/include/config.php
+++ Professional/assets/include/config.php
@@ -2,7 +2,7 @@
     $db_host = 'localhost';
     $db_username = 'root';
     $db_password = '';
-    $db_name = 'roshanb';
+    $db_name = 'student';
     
     $connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 ?>
