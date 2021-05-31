a8457f290f3040017b44bf632a0d549d591a1bb6
Roshan Budhathoki <47130986+Quicker123@users.noreply.github.com>
2021-05-31 17:20:48 +0545

    change
    changes

--- Professional/recentfunction.php
+++ Professional/recentfunction.php
@@ -1,6 +1,13 @@
 <?php
-function secure_sanitize()
-{}
+include "./assets/include/config.php";
+function secure_sanitize($input)
+{
+    $input = trim($input);
+    $input = htmlspecialchars($input);
+    echo $input;
+}
+secure_sanitize("<h1>I am roshan</h1>");
+//function that returns ip address and browser details
 function get_ip_browserdetail()
 {
     //whether ip is from the share internet
@@ -16,6 +23,20 @@ function get_ip_browserdetail()
         $ip_address = $_SERVER['REMOTE_ADDR'];
     }
     echo $ip_address;
+    echo $_SERVER['HTTP_USER_AGENT'];
 }
-get_ip_browserdetail();
-echo $_SERVER['HTTP_USER_AGENT'];
+// function that takes similar parameter and inserts data into db
+$data = array(
+    'name' => 'name',
+    'value' => 'value',
+);
+function insert_into_db($table, $data)
+{
+    global $connect;
+    $column = array_keys($data);
+    echo $column;
+    $values = json_encode(array_values($data));
+    echo $values;
+    mysqli_query($connect, "INSERT INTO '$table' ('$column') VALUES ('$values')");
+}
+?>
